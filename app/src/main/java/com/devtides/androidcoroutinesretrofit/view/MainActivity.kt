a7c60368d4b93f8d491f9d066ff31c9f74209daa
Marchael Acode <m.acode@outlook.com>
2021-04-11 17:37:09 -0700

    Using coroutines to retrieve data from the countries API and then use the data to updated our main UI with the countries name, flag, and capital.

--- app/src/main/java/com/devtides/androidcoroutinesretrofit/view/MainActivity.kt
+++ app/src/main/java/com/devtides/androidcoroutinesretrofit/view/MainActivity.kt
@@ -6,24 +6,24 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.devtides.androidcoroutinesretrofit.R
+import com.devtides.androidcoroutinesretrofit.databinding.ActivityMainBinding
 import com.devtides.androidcoroutinesretrofit.viewmodel.ListViewModel
 import com.devtides.coroutinesretrofit.view.CountryListAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var viewModel: ListViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: ListViewModel
     private val countriesAdapter = CountryListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
         viewModel.refresh()
 
-        countriesList.apply {
+        binding.countriesList.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = countriesAdapter
         }
@@ -31,23 +31,23 @@ class MainActivity : AppCompatActivity() {
         observeViewModel()
     }
 
-    fun observeViewModel() {
+    private fun observeViewModel() {
         viewModel.countries.observe(this, Observer {countries ->
             countries?.let {
-                countriesList.visibility = View.VISIBLE
+                binding.countriesList.visibility = View.VISIBLE
                 countriesAdapter.updateCountries(it) }
         })
 
         viewModel.countryLoadError.observe(this, Observer { isError ->
-            list_error.visibility = if(isError == "") View.GONE else View.VISIBLE
+            binding.listError.visibility = if(isError == "") View.GONE else View.VISIBLE
         })
 
         viewModel.loading.observe(this, Observer { isLoading ->
             isLoading?.let {
-                loading_view.visibility = if(it) View.VISIBLE else View.GONE
+                binding.loadingView.visibility = if(it) View.VISIBLE else View.GONE
                 if(it) {
-                    list_error.visibility = View.GONE
-                    countriesList.visibility = View.GONE
+                    binding.listError.visibility = View.GONE
+                    binding.countriesList.visibility = View.GONE
                 }
             }
         })
